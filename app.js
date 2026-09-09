da83c4417d182a4c7bc7dcb32501b24b7343a6fb
youapk89 <you.apk89@gmail.com>
2026-09-09 00:36:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
 // PanelStream - conexión Supabase
 
-const SUPABASE_URL = "https://tmfbecurjogbafxnnql.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_13TqMbS-zCkJIOyVIf06xw_pUgwTGNs";
+const SUPABASE_URL = 
+"https://tmfbecurjogbafxnnql.supabase.co";
+
+
+const SUPABASE_KEY = 
+"sb_publishable_13TqMbS-zCkJIOyVIf06xw_pUgwTGNs";
+
 
 const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
+SUPABASE_URL,
+SUPABASE_KEY
 );
 
-console.log("PanelStream conectado a Supabase");
 
-// Usuario administrador permitido
+// ÚNICO ADMINISTRADOR
+
 const ADMIN_EMAIL = "josimar8999@gmail.com";
 
-// Iniciar sesión
-async function login(email, password) {
-  if (email !== ADMIN_EMAIL) {
-    alert("Usuario no autorizado");
-    return;
-  }
 
-  const { data, error } = await supabaseClient.auth.signInWithPassword({
-    email,
-    password
-  });
 
-  if (error) {
-    alert(error.message);
-    return;
-  }
+// LOGIN
 
-  console.log("Sesión iniciada", data.user);
+async function login(){
+
+
+let email = document.getElementById("email").value;
+
+let password = document.getElementById("password").value;
+
+
+let mensaje = document.getElementById("mensaje");
+
+
+
+if(email !== ADMIN_EMAIL){
+
+mensaje.innerHTML = "Usuario no autorizado";
+
+return;
+
+}
+
+
+
+const {data,error} = await supabaseClient.auth.signInWithPassword({
+
+email: email,
+
+password: password
+
+});
+
+
+
+if(error){
+
+mensaje.innerHTML = error.message;
+
+return;
+
+}
+
+
+
+mensaje.innerHTML = "Ingreso correcto";
+
+
+document.querySelector(".card").style.display="none";
+
+
+document.getElementById("panel").style.display="block";
+
+
+
+console.log("Administrador conectado", data.user);
+
+
+
 }
