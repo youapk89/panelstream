0e17fcd4a24f3ec19463ba025d4d35be12f9d096
youapk89 <you.apk89@gmail.com>
2026-09-09 01:00:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,12 @@
-// PANELSTREAM PRUEBA SUPABASE
-
-const SUPABASE_URL = "https://tmfbecurjogbafxnnql.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_13TqMbS-zCkJIOyVIf06xw_pUgwTGNs";
-
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+console.log("APP.JS CARGADO CORRECTAMENTE");
 
 
 document.getElementById("estado").innerHTML =
-"Panel cargado correctamente";
+"App.js funcionando correctamente";
 
 
-async function probarConexion(){
+function probarConexion(){
 
-    let estado = document.getElementById("estado");
-
-    estado.innerHTML = "Probando conexión...";
-
-
-    const { data, error } = await supabaseClient
-    .from("servicios")
-    .select("*");
-
-
-    if(error){
-
-        console.log(error);
-
-        estado.innerHTML =
-        "Error conexión: " + error.message;
-
-        return;
-    }
-
-
-    console.log(data);
-
-
-    estado.innerHTML =
-    "Conectado a Supabase ✅<br>Total registros: " + data.length;
+alert("Botón funcionando correctamente");
 
 }
